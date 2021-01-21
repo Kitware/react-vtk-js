8af390aa7e03a2324ce0aa8cea77e8eead737889
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-01-20 17:50:06 -0700

    fix export

--- src/representations/index.js
+++ src/representations/index.js
@@ -1,8 +1,10 @@
 import vtkPointCloudRepresentation from './PointCloudRepresentation';
 import vtkVolumeDataRepresentation from './VolumeDataRepresentation';
 
+export const PointCloudRepresentation = vtkPointCloudRepresentation;
 export const VolumeDataRepresentation = vtkVolumeDataRepresentation;
 
 export default {
+  PointCloudRepresentation: vtkPointCloudRepresentation,
   VolumeDataRepresentation: vtkVolumeDataRepresentation,
 };
