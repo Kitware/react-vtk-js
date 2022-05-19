f985802b7c94a96b5f556d0f0a85a5b12aec52ec
Eduardo Reveles <me@osiux.ws>
2022-05-19 15:03:28 -0500

    add base url to vite build

--- usage/vite.config.js
+++ usage/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const base = process.env.GITHUB_ACTIONS ? '/react-vtk-js/' : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
   server: {
-    port: 9000
-  }
-})
+    port: 9000,
+  },
+});
