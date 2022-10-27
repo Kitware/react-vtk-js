a373b8ff84d970954454741d7510f6c900faabab
Forrest <forrest.li@kitware.com>
2022-10-27 17:56:59 -0400

    fix(ts/View): set camera props in correct order

--- src/core-ts/View/useCamera.ts
+++ src/core-ts/View/useCamera.ts
@@ -1,5 +1,6 @@
 import { ICameraInitialValues } from '@kitware/vtk.js/Rendering/Core/Camera';
 import vtkRenderer from '@kitware/vtk.js/Rendering/Core/Renderer';
+import { Vector3 } from '@kitware/vtk.js/types';
 import { useCallback } from 'react';
 import { compareShallowObject } from '../../utils-ts/comparators';
 import useComparableEffect from '../../utils-ts/useComparableEffect';
@@ -19,7 +20,20 @@ export default function useCamera(
       if (!cameraProps) return;
       const camera = getCamera();
       const mtime = camera.getMTime();
+
       camera.set(cameraProps);
+
+      // force-set focal point and direction of projection,
+      // as setting the position changes the focal point and dop
+      if (cameraProps.directionOfProjection) {
+        camera.setDirectionOfProjection(
+          ...(cameraProps.directionOfProjection as Vector3)
+        );
+      }
+      if (cameraProps.focalPoint) {
+        camera.setFocalPoint(...(cameraProps.focalPoint as Vector3));
+      }
+
       // camera.set doesn't return whether a change occurred,
       // since setPosition/etc. don't return this flag.
       if (mtime < camera.getMTime()) {
