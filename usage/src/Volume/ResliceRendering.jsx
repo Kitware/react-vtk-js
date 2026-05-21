5ee071886de1d25eb033c2a80b89d7215d55c073
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2026-05-20 23:14:20 -0400

    Fix ResliceRendering example for updated API

--- usage/src/Volume/ResliceRendering.jsx
+++ usage/src/Volume/ResliceRendering.jsx
@@ -2,12 +2,15 @@ import React, { useState, useContext } from 'react';
 import vtkColorMaps from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction/ColorMaps.js';
 import { SlabTypes } from '@kitware/vtk.js/Rendering/Core/ImageResliceMapper/Constants.js';
 import { InterpolationType } from '@kitware/vtk.js/Rendering/Core/ImageProperty/Constants.js';
+import vtkXMLImageDataReader from '@kitware/vtk.js/IO/XML/XMLImageDataReader.js';
 import { newInstance as newVtkCylinderInstance } from '@kitware/vtk.js/Filters/Sources/CylinderSource.js';
 import { newInstance as newVtkPlaneInstance } from '@kitware/vtk.js/Common/DataModel/Plane.js';
 
 import {
   View,
   ShareDataSet,
+  ShareDataSetRoot,
+  RegisterDataSet,
   ResliceRepresentation,
   Reader,
   Contexts,
@@ -32,7 +35,7 @@ function Slider(props) {
   const onChange = (e) => {
     const value = Number(e.currentTarget.value);
     props.setValue(value);
-    setTimeout(view.renderView, 0);
+    setTimeout(view.requestRender, 0);
   };
   return (
     <label
@@ -68,7 +71,7 @@ function DropDown(props) {
   function onChange(e) {
     const value = e.currentTarget.value;
     props.setValue(value);
-    setTimeout(view.renderView, 0);
+    setTimeout(view.requestRender, 0);
   }
   return (
     <label
@@ -105,7 +108,7 @@ function EnumDropDown(props) {
   function onChange(e) {
     const value = parseInt(e.currentTarget.value);
     props.setValue(value);
-    setTimeout(view.renderView, 0);
+    setTimeout(view.requestRender, 0);
   }
   return (
     <label
@@ -146,7 +149,7 @@ function CheckBox(props) {
   function onChange(e) {
     const value = e.currentTarget.checked;
     props.setValue(value);
-    setTimeout(view.renderView, 0);
+    setTimeout(view.requestRender, 0);
   }
   return (
     <label
@@ -183,13 +186,11 @@ function SliceFunction(props) {
       cyl.update();
       props.setSlicePolyData(cyl.getOutputData());
     } else {
-      // plane.update();
-      // props.setSlicePolyData(plane.getOutputData());
       props.setSlicePolyData(null);
       props.setSlicePlane(plane);
     }
     props.setValue(value);
-    setTimeout(view.renderView, 0);
+    setTimeout(view.requestRender, 0);
     setTimeout(view.resetCamera, 0);
   }
   return (
@@ -219,9 +220,8 @@ function SliceFunction(props) {
 }
 
 function PosSlider(props) {
-  const s = Slider(props);
   plane.setOrigin(props.value, 0, 0);
-  return s;
+  return Slider(props);
 }
 
 function Example(props) {
@@ -243,132 +243,129 @@ function Example(props) {
   const [slicePos, setSlicePos] = useState(127);
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <div style={{ width: '50vw', height: '100%', display: 'inline-block' }}>
-        <View
-          id='0'
-          cameraPosition={[1, 0, 0]}
-          cameraViewUp={[0, 0, -1]}
-          cameraParallelProjection={false}
-          background={[0.34, 0.35, 0.34]}
-        >
-          <ShareDataSet>
-            <Reader
-              vtkClass='vtkXMLImageDataReader'
-              url='https://data.kitware.com/api/v1/item/59e12e988d777f31ac6455c5/download'
-            />
-          </ShareDataSet>
-          <Slider
-            label='colorLevel'
-            max={4095}
-            value={colorLevel}
-            setValue={setColorLevel}
-            style={{ top: '30px', left: '5px' }}
+      <ShareDataSetRoot>
+        <RegisterDataSet id='mainDataset'>
+          <Reader
+            vtkClass={vtkXMLImageDataReader}
+            url='https://data.kitware.com/api/v1/item/59e12e988d777f31ac6455c5/download'
           />
-          <Slider
-            label='colorWindow'
-            max={4095}
-            value={colorWindow}
-            setValue={setColorWindow}
-            style={{ top: '30px', left: '255px' }}
-          />
-          <EnumDropDown
-            options={{
-              Linear: InterpolationType.LINEAR,
-              Nearest: InterpolationType.NEAREST,
-            }}
-            label='interpolationType'
-            value={interpolationType}
-            setValue={setInterpolationType}
-            style={{ top: '30px', left: '505px' }}
-          />
-          <DropDown
-            label='lookupTable'
-            options={vtkColorMaps.rgbPresetNames}
-            value={colorPreset}
-            setValue={setColorPreset}
-            style={{ top: '60px', left: '5px' }}
-          />
-          <CheckBox
-            label='useLookupTableScalarRange'
-            value={useLookupTableScalarRange}
-            setValue={setUseLookupTableScalarRange}
-            style={{ top: '60px', left: '505px' }}
-          />
-          <Slider
-            label='slabThickness'
-            max={10}
-            value={slabThickness}
-            setValue={setSlabThickness}
-            style={{ top: '90px', left: '5px' }}
-          />
-          <EnumDropDown
-            options={{
-              MIN: SlabTypes.MIN,
-              MAX: SlabTypes.MAX,
-              MEAN: SlabTypes.MEAN,
-              SUM: SlabTypes.SUM,
-            }}
-            label='slabMode'
-            value={slabType}
-            setValue={setSlabType}
-            style={{ top: '90px', left: '255px' }}
-          />
-          <CheckBox
-            label='slabTrapezoidIntegration'
-            value={slabTrapezoidIntegration}
-            setValue={setSlabTrapezoidIntegration}
-            style={{ top: '90px', left: '505px' }}
-          />
-          <SliceFunction
-            label='SliceFunction'
-            value={usePolyData}
-            setValue={setUsePolyData}
-            setSlicePolyData={setSlicePolyData}
-            setSlicePlane={setSlicePlane}
-            style={{ top: '5px', left: '5px' }}
-          />
-          <PosSlider
-            label='slicePosition'
-            max={255}
-            value={slicePos}
-            setValue={setSlicePos}
-            // setSlicePolyData={setSlicePolyData}
-            style={{ top: '5px', left: '255px' }}
-          />
-          <ResliceRepresentation
-            slabThickness={slabThickness}
-            slabType={slabType}
-            slabTrapezoidIntegration={slabTrapezoidIntegration}
-            slicePolyData={slicePolyData}
-            slicePlane={slicePlane}
-            property={{
-              colorWindow,
-              colorLevel,
-              useLookupTableScalarRange,
-              interpolationType,
-            }}
-            colorMapPreset={colorPreset}
+        </RegisterDataSet>
+        <div style={{ width: '50vw', height: '100%', display: 'inline-block' }}>
+          <View
+            id='0'
+            camera={{ position: [1, 0, 0], viewUp: [0, 0, -1], parallelProjection: false }}
+            background={[0.34, 0.35, 0.34]}
           >
-            <ShareDataSet />
-          </ResliceRepresentation>
-        </View>
-      </div>
-      <div style={{ width: '50vw', height: '100%', display: 'inline-block' }}>
-        <View
-          id='0'
-          background={[0, 0, 0]}
-          cameraPosition={[1, 0, 0]}
-          cameraViewUp={[0, 0, -1]}
-          cameraParallelProjection={false}
-        >
-          <VolumeRepresentation>
-            <div style={{ display: 'none' }}>
-              <VolumeController />
-            </div>
-            <ShareDataSet />
-          </VolumeRepresentation>
-        </View>
-      </div>
+            <Slider
+              label='colorLevel'
+              max={4095}
+              value={colorLevel}
+              setValue={setColorLevel}
+              style={{ top: '30px', left: '5px' }}
+            />
+            <Slider
+              label='colorWindow'
+              max={4095}
+              value={colorWindow}
+              setValue={setColorWindow}
+              style={{ top: '30px', left: '255px' }}
+            />
+            <EnumDropDown
+              options={{
+                Linear: InterpolationType.LINEAR,
+                Nearest: InterpolationType.NEAREST,
+              }}
+              label='interpolationType'
+              value={interpolationType}
+              setValue={setInterpolationType}
+              style={{ top: '30px', left: '505px' }}
+            />
+            <DropDown
+              label='lookupTable'
+              options={vtkColorMaps.rgbPresetNames}
+              value={colorPreset}
+              setValue={setColorPreset}
+              style={{ top: '60px', left: '5px' }}
+            />
+            <CheckBox
+              label='useLookupTableScalarRange'
+              value={useLookupTableScalarRange}
+              setValue={setUseLookupTableScalarRange}
+              style={{ top: '60px', left: '505px' }}
+            />
+            <Slider
+              label='slabThickness'
+              max={10}
+              value={slabThickness}
+              setValue={setSlabThickness}
+              style={{ top: '90px', left: '5px' }}
+            />
+            <EnumDropDown
+              options={{
+                MIN: SlabTypes.MIN,
+                MAX: SlabTypes.MAX,
+                MEAN: SlabTypes.MEAN,
+                SUM: SlabTypes.SUM,
+              }}
+              label='slabMode'
+              value={slabType}
+              setValue={setSlabType}
+              style={{ top: '90px', left: '255px' }}
+            />
+            <CheckBox
+              label='slabTrapezoidIntegration'
+              value={slabTrapezoidIntegration}
+              setValue={setSlabTrapezoidIntegration}
+              style={{ top: '90px', left: '505px' }}
+            />
+            <SliceFunction
+              label='SliceFunction'
+              value={usePolyData}
+              setValue={setUsePolyData}
+              setSlicePolyData={setSlicePolyData}
+              setSlicePlane={setSlicePlane}
+              style={{ top: '5px', left: '5px' }}
+            />
+            <PosSlider
+              label='slicePosition'
+              max={255}
+              value={slicePos}
+              setValue={setSlicePos}
+              style={{ top: '5px', left: '255px' }}
+            />
+            <ResliceRepresentation
+              slabThickness={slabThickness}
+              slabType={slabType}
+              slabTrapezoidIntegration={slabTrapezoidIntegration}
+              slicePolyData={slicePolyData}
+              slicePlane={slicePlane}
+              property={{
+                colorWindow,
+                colorLevel,
+                useLookupTableScalarRange,
+                interpolationType,
+              }}
+              colorMapPreset={colorPreset}
+            >
+              <ShareDataSet id='mainDataset' />
+            </ResliceRepresentation>
+          </View>
+        </div>
+        <div style={{ width: '50vw', height: '100%', display: 'inline-block' }}>
+          <View
+            id='0'
+            background={[0, 0, 0]}
+            camera={{ position: [1, 0, 0], viewUp: [0, 0, -1], parallelProjection: false }}
+          >
+            <VolumeRepresentation>
+              <div style={{ display: 'none' }}>
+                <VolumeController />
+              </div>
+              <ShareDataSet id='mainDataset' />
+            </VolumeRepresentation>
+          </View>
+        </div>
+      </ShareDataSetRoot>
     </div>
   );
 }
